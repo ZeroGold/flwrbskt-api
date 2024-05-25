294f1d9040ad04dcfa8f545a68399dd3af88e9e4
ZeroGold <77712091+ZeroGold@users.noreply.github.com>
2024-05-25 18:41:55 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-from time import time
-from fastapi import FastAPI, __version__
-#from fastapi.staticfiles import StaticFiles
-from fastapi.responses import HTMLResponse
-
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -11,11 +6,6 @@ app = FastAPI()
 @app.get('/')
 def hello_world():
     return "Hello,World"
-
-
-@app.get('/ping')
-async def hello():
-    return {'res': 'pong', 'version': __version__, "time": time()}
 
 
 if __name__ == '__main__':
